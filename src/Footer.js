6c47c11f996436f7a2bb22a13e1e1ec921daabc2
Thony Nava <thonynava@Thonys-MacBook-Pro.local>
2020-10-27 20:18:15 +0100

    css update

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 let Footer = props => {
   return (
-    <ul>
+    <ul className="text-center mt-4 pt-4">
       {props.arr.map((element, i) => {
         return <li key={i}>{element}</li>;
       })}
--- src/Heading.js
+++ src/Heading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 let Heading = () => (
   <div>
-    <header className="App-header">
+    <header className="App-header mb-4">
       <img src="https://image.flaticon.com/icons/svg/139/139899.svg" />
       <h3>CYF Hotel</h3>
     </header>
